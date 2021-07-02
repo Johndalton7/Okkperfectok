65911fef79fd0530eec273c2e376b8906dac3a3d
Johndalton7 <86659836+Johndalton7@users.noreply.github.com>
2021-07-02 16:20:51 +0530

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -38,7 +38,7 @@ async def start(bot, update):
                         [
                             InlineKeyboardButton
                                 (
-                                    'Developers', url="https://t.me/CrazyBotsz"
+                                    'JOIN CHANNEL', url="https://t.me/MFMOVIES3"
                                 )
                         ]
                     ]
@@ -57,7 +57,7 @@ async def start(bot, update):
                         [
                             InlineKeyboardButton
                                 (
-                                    'Developers', url="https://t.me/CrazyBotsz"
+                                    'JOIN CHANNEL', url="https://t.me/MFMOVIES3"
                                 )
                         ]
                     ]
@@ -76,7 +76,7 @@ async def start(bot, update):
                         [
                             InlineKeyboardButton
                                 (
-                                    'Developers', url="https://t.me/CrazyBotsz"
+                                    'JOIN CHANNEL', url="https://t.me/MFMOVIES3"
                                 )
                         ]
                     ]
@@ -89,10 +89,10 @@ async def start(bot, update):
         return
 
     buttons = [[
-        InlineKeyboardButton('Developers', url='https://t.me/CrazyBotsz'),
-        InlineKeyboardButton('Source Code 🧾', url ='https://github.com/CrazyBotsz/Adv-Auto-Filter-Bot-V2')
+        InlineKeyboardButton('OWNER', url='https://t.me/Johndalton_TG'),
+        InlineKeyboardButton('CHANNEL', url ='https://t.me/MFMOVIES3')
     ],[
-        InlineKeyboardButton('Support 🛠', url='https://t.me/CrazyBotszGrp')
+        InlineKeyboardButton('Support 🛠', url='https://t.me/MF_CHATGROUP')
     ],[
         InlineKeyboardButton('Help ⚙', callback_data="help")
     ]]
