0a3321c8cc49a77c200e49c4590a6789dcfb6822
Johndalton7 <86659836+Johndalton7@users.noreply.github.com>
2021-07-02 16:42:43 +0530

    Update translation.py

--- bot/translation.py
+++ bot/translation.py
@@ -6,11 +6,9 @@ class Translation(object):
     
     START_TEXT = """<b>Hey {}!!</b>
 <i>Am Just A Advance Auto Filter Bot....😉
+For MOVIEFACTORY GROUP
 
-Just Add Me To Your Group And Channel And Connect Them And See My Pevers 🔥🔥😝
-
-For More Details Click Help Button Below..
-@CrazyBotsz
+@MF_CHATGROUP
 </i>"""    
     
     HELP_TEXT = """
@@ -43,12 +41,12 @@ For More Details Click Help Button Below..
             
             -> <code>Status</code> - Button Will Shows The Stats Of Your Channel
             
-@CrazyBotsz
+@MF_CHATGROUP
 """
     
     ABOUT_TEXT = """<b>➥ Name</b> : <code> Auto Filter Bot</code>
     
-<b>➥ Creator</b> : <b><i><a href="https://t.me/AlbertEinstein_TG">AlbertEinstein_TG</a></i></b>
+<b>➥ Creator</b> : <b><i><a href="https://t.me/Johndalton_TG">AlbertEinstein_TG</a></i></b>
 
 <b>➥ Language</b> : <code>Python3</code>
 
